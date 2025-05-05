9ff5c5230cb0a7903f9b86c8b7624fbc31e9907c
MaddalenaRen <ileniarennella@gmail.com>
2025-05-05 16:25:11 +0200

    Create Esercizio3.java

---
+++ src/Esercizio3.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Esercizio3 {
+    public static double perimetroRettangolo(double lato1, double lato2) {
+        return 2 * (lato1 + lato2);
+    }
+    public static int pariDispari(int numero) {
+        return numero % 2 == 0 ? 0 : 1;
+    }
+}
